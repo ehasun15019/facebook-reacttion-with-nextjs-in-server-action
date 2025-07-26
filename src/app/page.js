609cba32171241b244ddfd584e8c8b525b5ed511
Your Full Name <ehasun0909@gamil.com>
2025-07-26 16:25:05 +0600

    added db.json in server folder with json-server

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,5 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 export default function Home() {
-  return (
-    <div>
-      <i class="fa-solid fa-heart"></i>
-    </div>
-  );
+  return <div></div>;
 }
